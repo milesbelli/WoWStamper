bd02ec265c34e75bb173d6ee0f4408a9ad2bea16
milesbelli <lmitas@outlook.com>
2016-01-11 22:52:57 -0500

    First working draft. It works, it's not pretty, and it is very specifically tailored to my filesystem.
    But hey, it works!

--- README.md
+++ README.md
@@ -3,8 +3,16 @@ Script to batch add exif data to WoW screenshots.
 
 Primarily, this project will be focused on:
 
-* Reading Timestamp from screenshot filename
+* Parsing Timestamp from screenshot filename
 * Writing the "Date Taken" exif data to the file
 
 As of now the idea is to run this once, but in the future this script could be modified to track which files have already been converted, and only update new files. This could even be set to run with the Windows Task Scheduler so that new screenshots are timestamped periodically.
 
+In the even further future, it may be possible to add additional exif data, possibly using OCR, to add tags based on text we know will be in certain areas on screen (such as zone name beneath minimap).
+
+
+* Some Disclaimers*
+
+This was written on Windows (sorry). As a result, you should be able to run it without issue on your machine, but with file IO being an integral part of this, I have no idea if any of it will work in other OSes.
+
+The script works, but it's very rudimentary, and right now the code needs to be modified to point at the appropriate directory, and it is specifically tailored for WoW screenshots.
--- converter.py
+++ converter.py
@@ -2,3 +2,52 @@
 # Instructions to accomplish this can be found here:
 # http://smarnach.github.io/pyexiftool/
 import exiftool
+from pathlib import Path
+
+def writeExif(dateTime,targetPath):
+    with exiftool.ExifTool() as et:
+        
+        # For reference, EXIF:CreateDate format is: 2014:09:02 10:58:53
+        
+        addDate = '-CreateDate=' + dateTime
+        params = [bytearray(addDate, 'utf-8'),bytearray(targetPath, 'utf-8')]
+        
+        #print(params)
+        et.execute(*params)
+        #metadata = et.get_metadata(targetPath)
+        #print(metadata)
+        
+def parseDateTime(name,prefix):
+    
+    #name is required, prefix is optional
+    #future task: change prefix to list of things to remove, iterate through
+    
+    #lot of crap to get rid of, I need to tidy this up
+    s = name.replace(prefix,'')
+    s = s.replace('_','')
+    s = s.replace('.','')
+    s = s.replace('jpg','')
+    #print(s)
+    
+    if(s.isnumeric() == True):
+        
+        formattedDate = ''.join(['20', s[4:6], ':', s[0:2], ':', s[2:4], ' ', s[6:8], ':', s[8:10], ':', s[10:12]])
+        
+        return (formattedDate)
+        
+
+screenshotDir = 'E:\\ProgFiles WoW\\World of Warcraft\\test\\'
+dirPath = Path(screenshotDir)
+filePrefix = 'WoWScrnShot_'
+
+if(dirPath.exists()):
+    print('Now converting...')
+    for file in dirPath.iterdir():
+        strFile = str(file)
+        fileName = strFile.replace(screenshotDir,'')
+        
+        dateTime = parseDateTime(fileName,filePrefix)
+        dateTime = str(dateTime)
+        writeExif(dateTime,strFile)
+
+
